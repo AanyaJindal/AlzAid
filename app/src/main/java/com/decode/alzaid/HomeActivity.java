df2e2b60a37a8db2f25fab24d0ad8bb86a8d3f9e
A-D-I-T-Y-A <aditya224gupta@gmail.com>
2017-02-11 04:29:34 +0530

    HomeActivity Update 1

--- app/src/main/java/com/decode/alzaid/HomeActivity.java
+++ app/src/main/java/com/decode/alzaid/HomeActivity.java
@@ -2,12 +2,29 @@ package com.decode.alzaid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
+
+    String[] itemList = {"Home","Call Home","Quiz","New Question"};
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        List<String> st = Arrays.asList(itemList);
+        recyclerView = (RecyclerView) findViewById(R.id.home_recycler_view);
+        RecyclerView.Adapter mAdapter = new HomeAdapter(st,getApplicationContext());
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getApplicationContext(),2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setAdapter(mAdapter);
+
+
     }
 }
