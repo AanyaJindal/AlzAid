d7d38ca6a84b0972c0d52e6a06cf07a04f9df86f
A-D-I-T-Y-A <aditya224gupta@gmail.com>
2017-02-11 08:13:26 +0530

    new

--- app/src/main/java/com/decode/alzaid/QuestionDetailActivity.java
+++ app/src/main/java/com/decode/alzaid/QuestionDetailActivity.java
@@ -10,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.github.florent37.materialtextfield.MaterialTextField;
+
 public class QuestionDetailActivity extends AppCompatActivity {
 
     private static final String TAG = "QuestionDetailActivity";
@@ -17,6 +19,7 @@ public class QuestionDetailActivity extends AppCompatActivity {
     Spinner spQuestions;
     EditText etAnswer, etOption1, etOption2, etOption3, etCustomQuestion;
     Button btnAddQuestion;
+    MaterialTextField mt;
     String ques, ans, op1, op2, op3;
 
     @Override
@@ -31,17 +34,24 @@ public class QuestionDetailActivity extends AppCompatActivity {
         etOption2 = (EditText) findViewById(R.id.et_option2);
         etOption3 = (EditText) findViewById(R.id.et_option3);
         btnAddQuestion = (Button) findViewById(R.id.btn_add_question);
+        mt = (MaterialTextField)findViewById(R.id.et_custom_case);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.questions_array, android.R.layout.simple_spinner_item);
         spQuestions.setAdapter(adapter);
 
-
+        final String[] itm = getResources().getStringArray(R.array.questions_array);
         spQuestions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
                 Object item = parent.getItemAtPosition(pos);
-                ques = item.toString();
+                ques = itm[pos];
                 if(ques.equals("Custom Question")){
+                    Log.d("custom",ques);
+                    mt.setVisibility(View.VISIBLE);
                     etCustomQuestion.setVisibility(View.VISIBLE);
                     ques = etCustomQuestion.getText().toString();
+                }
+                else{
+                    etCustomQuestion.setVisibility(View.GONE);
+                    mt.setVisibility(View.GONE);
                 }
             }
             public void onNothingSelected(AdapterView<?> parent) {
